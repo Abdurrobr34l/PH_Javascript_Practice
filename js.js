08b7206f7a0b7f39adcdc56ec70abc4e15781c76
AbdurRob <abdurrobr34l@gmail.com>
2025-08-01 12:28:50 +0600

    Added string question from ph github

--- js.js
+++ js.js
@@ -1,2 +1,18 @@
-let a = 0
-console.log(a);
+//* Problem 1: Student Result System
+// You're building a system for a school to manage student results.
+
+let student = {
+  name: "Sakib Khan",
+  age: 17,
+  class: 10,
+  marks: {
+    math: 88,
+    english: 76,
+    science: 92
+  }
+};
+
+//? Tasks:
+// Check if the student has passed all subjects (passing mark is 40).
+// Calculate and print the average marks.
+// If average > 85, print "Grade: A", else if > 70 "Grade: B", else "Grade: C".
---
+++ ph-object-questions/ph-questions(String).js
@@ -0,0 +1,14 @@
+//? Task-1:
+// Count how many times a string has the letter a
+
+//? Task-2:
+// Count how many times a string has the letter a or A
+
+//? Task-3:
+// Check whether a string contains all the vowels a, e, i, o, u
+
+//? Task-4:
+// If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+
+//? Task-5:
+// Capitalize Every first Letter of each word in a String
