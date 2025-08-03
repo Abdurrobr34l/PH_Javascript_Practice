52c887b48dd96343b8cf2f5dff4a31c195754ff2
AbdurRob <abdurrobr34l@gmail.com>
2025-08-03 17:11:35 +0600

    Made seperate folder for first 7 chapter practice questions and mini-porject

--- js.js
+++ js.js
@@ -1,3 +1,3 @@
 // *Mini Project: Simple Product Inventory App
 // ?🛒 Goal:
-// You will build an app to track products in a store, check stock levels, total value, and allow search by product name.
+// ?You will build an app to track products in a store, check stock levels, total value, and allow search by product name.
