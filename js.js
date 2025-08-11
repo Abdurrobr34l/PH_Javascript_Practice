0015638464e4d0dbcafd82730540a70f137ab4b5
AbdurRob <abdurrobr34l@gmail.com>
2025-08-11 20:40:46 +0600

    Completed 2 practice task questions given in module-20

---
+++ js.js
@@ -0,0 +1,39 @@
+// //* Task-1: Celsius to Fahrenheit
+// function tempFahrenheit(celsius) {
+//     let fahrenheit = 32;
+//     let tempConvertion = (celsius * 9/5) + fahrenheit;  // (0°C × 9/5) + 32
+//     return tempConvertion;
+// }
+
+// let tempConvertionResult = tempFahrenheit(1) + "°F";
+// console.log(tempConvertionResult);
+
+
+// //* Task-2: Count occurrences of a number in an array
+// function numChecker(numbersArray) {
+//     let findNum = 5;
+//     let countNum = 0;
+//     for (const number of numbersArray) {
+//         if (number === findNum) {
+//             countNum++;
+//         }
+//     }
+//     return countNum;
+// }
+
+// let numCheckerResult = numChecker([5, 6, 11, 12, 98, 5]);
+// console.log(numCheckerResult);  // output: 2
+
+
+//* Task-3:
+// Write a function to count the number of vowels in a string.
+
+//* Task-4:
+// Write a function to find the longest word in a given string.
+
+// sample-input: I am learning Programming to become a programmer
+
+// sample-output: Programming
+
+//* Task-5:
+// Generate a random number between 10 to 20.
---
+++ module-20/ph-practice_questions.js
@@ -0,0 +1,2 @@
+//* Here is the GitHub Repo - https://github.com/ProgrammingHero1/js-problems-part1-practice-tasks.git
+//* IT IS MODULE-20 PRACTICE TASK
